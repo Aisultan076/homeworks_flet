bc92c2880cbb71346fe4970c793dce1453a70e21
Aisultan <ajsultankockorbajuuluajsultan@gmail.com>
2025-05-12 15:42:29 +0600

    hw_3

--- src/name.py
+++ src/name.py
@@ -1,55 +1,29 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Ваши расходы"
-    page.data = 0
 
+def main(page: ft.Page):
+    page.title = "Учет расходов"
+    page.verical_alignment = ft.MainAxisAlingment.CENTER
 
     name_input = ft.TextField(label="Название расхода")
-    amount_input = ft.TextField(label="Сумма расхода(сом)", keyboard_type="number")
-
-
-    total_text = ft.Text(value="Общая сумма: 0 сом", size=20, weight="bold")
-
-
-    expenses_list = ft.Column()
-
+    amount_input = ft.TextField(label="Сумма расхода")
+    result = ft.Text()
 
     def add_expense(e):
-        name = name_input.value
-        try:
-            amount = float(amount_input.value)
-        except ValueError:
-            page.snack_bar = ft.SnackBar(ft.Text("Введите корректную сумму!"))
-            page.snack_bar.open = True
-            page.update()
-            return
-
-
-        expenses_list.controls.append(ft.Text(f"{name}: {amount}"))
-
-
-        page.data += amount
-        total_text.value = f"Общая сумма расходов: {page.data} сом"
-
-
-        name_input.value = ""
-        amount_input.value = ""
-
-
+        if name_input.value and amount_input.value:
+            result.value = f"Добавлено: {name_input.value}"
+        else:
+            result.value = "Заполните оба поля"
         page.update()
-
 
     add_button = ft.ElevatedButton(text="Добавить", on_click=add_expense)
 
-
     page.add(
+        ft.Text("Учет расходов", size=30, weight=ft.FontWeight.BOLD),
         name_input,
         amount_input,
         add_button,
-        total_text,
-        ft.Text("Список расходов:"),
-        expenses_list,
+        result
     )
 
 ft.app(target=main)
